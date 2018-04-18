631fa78b18c6d9745cf766a63e681e12d9b2cb97
macbook <tsulukiya86@gmail.com>
2018-04-18 13:09:46 -0700

    init commit...

---
+++ src/lesson7/DbConnector.java
@@ -0,0 +1,12 @@
+package lesson7;
+
+public class DbConnector {
+    String host;
+    int port;
+    String userName;
+    String password;
+
+    void connect() {
+
+    }
+}
---
+++ src/lesson7/Demo.java
@@ -0,0 +1,12 @@
+package lesson7;
+
+public class Demo {
+    public static void main(String[] args) {
+        User user = new User();
+        User user1 = new User();
+        User user2 = new User();
+        Car car = new Car(10000, 2015, "Anton");
+        DbConnector dbConnector = new DbConnector();
+
+    }
+}

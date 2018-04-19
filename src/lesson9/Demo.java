087665745b43c7f19796985b5027332d5fa03b96
macbook <tsulukiya86@gmail.com>
2018-04-19 11:27:01 -0700

    lesson 9 (part1) commit...

---
+++ src/lesson9/Demo.java
@@ -0,0 +1,19 @@
+package lesson9;
+
+import lesson9.utils.Checker;
+
+public class Demo {
+    public static void main(String[] args) {
+     /*   Company company = new Company();
+        System.out.println(company.gcountryFounded);
+        System.out.println(company.getName());
+
+        company.name = "NewValue";
+
+        company.name = "IBM";
+
+        Checker checker = new Checker();
+        System.out.println(checker.checkCompanyName(company.name));*/
+        //System.out.println(checker.companyNamesValidateCount);
+    }
+}

6f3a2ce4143e5471e7d43ffb53d9c7f1a7d7c8d1
macbook <tsulukiya86@gmail.com>
2018-04-21 07:09:01 -0700

    lesson 9 (part5) final commit...

--- src/lesson9/userRepository/UserRepository.java
+++ src/lesson9/userRepository/UserRepository.java
@@ -129,7 +129,8 @@ public class UserRepository {
 
     public User update(User user) {
         User user1 = findById(user.getId());
-        if (user1 == null) {
+        
+        if (user1 != null) {
 
             for (int i = 0; i < users.length; i++) {
 

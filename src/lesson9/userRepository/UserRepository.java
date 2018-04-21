332bffd2f44402870041017ab228aace0ef460cc
macbook <tsulukiya86@gmail.com>
2018-04-21 04:32:00 -0700

    lesson 9 (part2) final commit...

--- src/lesson9/userRepository/UserRepository.java
+++ src/lesson9/userRepository/UserRepository.java
@@ -105,22 +105,17 @@ public class UserRepository {
     public User save(User user) {
         User user1 = null;
 
-        if (user != null) {
+        if ((findById(user.getId()) != null) || (users.length == count())) {
+            user1 = null;
+        } else {
+            for (User user2 : users) {
 
-
-            if ((findById(user.getId()) != null) || (users.length == count())) {
-                user1 = null;
-            } else {
-                for (User user2 : users) {
-
-                    if (user2 == null) {
-                        user2 = user;
-                        user1 = user2;
-                    }
+                if ((user != null) && (user2 == null)) {
+                    user2 = user;
+                    user1 = user2;
                 }
             }
         }
-
         return user1;
     }
 

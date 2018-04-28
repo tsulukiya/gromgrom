713e76e1cad00d963a2c0438b56efbc88b9a845f
macbook <tsulukiya86@gmail.com>
2018-04-27 23:27:01 -0700

    lesson 10 (HomeWork) commit (Customer, Order, ElectronicsOrder)

--- src/lesson10/HomeWorkLesson10/ElectronicsOrder.java
+++ src/lesson10/HomeWorkLesson10/ElectronicsOrder.java
@@ -12,13 +12,11 @@ public class ElectronicsOrder extends Order {
 
     @Override
     public void validateOrder() {
-        if ((getShipFromCity() == "Киев" || getShipFromCity() == "Одесса" || getShipFromCity() == "Харьков") &
-                (getShipToCity() == "Киев" || getShipToCity() == "Одесса" || getShipToCity() == "Харьков") &
-                (getBasePrice() >= 100) & (getCustomerOwned().getGender() == "Женский")) {
+        if ((getShipFromCity() == "Киев" || getShipFromCity() == "Одесса" || getShipFromCity() == "Харьков") &&
+                (getShipToCity() == "Киев" || getShipToCity() == "Одесса" || getShipToCity() == "Харьков") &&
+                (getBasePrice() >= 100) & (getCustomerOwned().getGender() == "Женский"))
 
             confirmShipping();
-
-        }
 
 
     }

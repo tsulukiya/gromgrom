3535914469474033e35a2e1692f7755313509c71
macbook <tsulukiya86@gmail.com>
2018-04-30 11:34:25 -0700

    lesson 10 (HomeWork) commit (Customer, Order, ElectronicsOrder)

--- src/lesson11/homework/BookingComAPI.java
+++ src/lesson11/homework/BookingComAPI.java
@@ -11,41 +11,35 @@ public class BookingComAPI implements API {
     @Override
     public Room[] findRooms(int price, int person, String city, String hotel) {
         int index = 0;
+        Room[] rooms1 = new Room[rooms.length];
         int priceMin = price - 100;
         int priceMax = price + 100;
-        if (price <= 100)
+        if (price <= 100) {
             priceMin = 0;
+        }
         for (int i = 0; i < rooms.length; i++) {
 
             if (rooms[i] != null) {
 
-                if (rooms[i].getPrice() == price && rooms[i].getPersons() == person && rooms[i].getCityName() == city && rooms[i].getHotelName() == hotel) {
-                    index = index + 1;
-
-                } else if ((rooms[i].getPrice() >= priceMin) && (rooms[i].getPrice() <= priceMax)) {
-                    index = index + 1;
-
+                if ((rooms[i].getPrice() == price && rooms[i].getPersons() == person &&
+                        rooms[i].getCityName() == city && rooms[i].getHotelName() == hotel) ||
+                        ((rooms[i].getPrice() >= priceMin) && (rooms[i].getPrice() <= (priceMax)))) {
+                    rooms1[i] = rooms[i];
+                    index++;
                 }
-
             }
         }
-        Room[] rooms1 = new Room[index];
-        for (int i = 0; i < rooms.length; i++) {
-            if (rooms[i] != null) {
 
-                if (rooms[i].getPrice() == price && rooms[i].getPersons() == person && rooms[i].getCityName() == city && rooms[i].getHotelName() == hotel) {
-                    rooms1[i] = rooms[i];
-
-                } else if ((rooms[i].getPrice() >= priceMin) && (rooms[i].getPrice() <= priceMax)) {
-                    rooms1[i] = rooms[i];
-
+        if (index > 0) {
+            Room [] rooms2 = new Room[index];
+            for (int i = 0; i < rooms1.length; i++) {
+                if (rooms1[i]!=null) {
+                    rooms2[i] = rooms1[i];
                 }
-
             }
-
+            return rooms2;
         }
-        return rooms1;
-
+        return null;
 
     }
 

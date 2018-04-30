6983e0901fbc9d323a8848102a5a2cce278fbda3
macbook <tsulukiya86@gmail.com>
2018-04-30 06:33:23 -0700

    lesson 10 (HomeWork) commit (Customer, Order, ElectronicsOrder)

---
+++ src/lesson11/FirstIntarface.java
@@ -0,0 +1,8 @@
+package lesson11;
+
+public interface FirstIntarface {
+
+    void send();
+
+    String receive();
+}
---
+++ src/lesson11/TestClass.java
@@ -0,0 +1,17 @@
+package lesson11;
+
+public class TestClass implements FirstIntarface {
+
+    private int test = 10;
+
+    @Override
+    public void send() {
+        // some logic
+    }
+
+    @Override
+    public String receive() {
+        // some logic
+        return null;
+    }
+}

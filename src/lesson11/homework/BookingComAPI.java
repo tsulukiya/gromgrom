501c2d24e8b124823721047c5c339adef6ea8d8a
macbook <tsulukiya86@gmail.com>
2018-04-30 12:54:25 -0700

    lesson 10 (HomeWork) commit (Customer, Order, ElectronicsOrder)

--- src/lesson11/homework/BookingComAPI.java
+++ src/lesson11/homework/BookingComAPI.java
@@ -10,23 +10,22 @@ public class BookingComAPI implements API {
 
     @Override
     public Room[] findRooms(int price, int persons, String city, String hotel) {
-        // int index = 0;
+        int index = 0;
         Room[] rooms1 = new Room[rooms.length];
 
         for (int i = 0; i < rooms.length; i++) {
 
-            if (rooms[i] != null) {
+            if ((rooms[i] != null) && (price >= 0)) {
 
-                if ((rooms[i].getPrice() >= (price - 100) && rooms[i].getPrice() <= (price + 100)) || ((rooms[i].getPersons() == persons) &&
+                if ((rooms[i].getPrice() >= 0) && (rooms[i].getPrice() >= (price - 100) && rooms[i].getPrice() <= (price + 100)) || ((rooms[i].getPersons() == persons) &&
                         (rooms[i].getCityName() == city) && (rooms[i].getHotelName() == hotel))) {
                     rooms1[i] = rooms[i];
-                    //index++;
+                    index++;
                 }
             }
         }
-        return rooms1;
 
-    /*    Room[] rooms2 = new Room[index];
+        Room[] rooms2 = new Room[index];
         for (int i = 0; i < rooms1.length; i++) {
             if (rooms1[i] != null) {
                 rooms2[index - 1] = rooms1[i];
@@ -34,7 +33,7 @@ public class BookingComAPI implements API {
 
             }
         }
-        return rooms2;*/
+        return rooms2;
 
 
     }

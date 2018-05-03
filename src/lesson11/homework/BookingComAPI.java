74a25ad85d776e0dd065dcd5b301a79a09a11b95
macbook <tsulukiya86@gmail.com>
2018-05-03 10:52:14 -0700

    Lesson11(HomeWork)...

--- src/lesson11/homework/BookingComAPI.java
+++ src/lesson11/homework/BookingComAPI.java
@@ -17,18 +17,17 @@ public class BookingComAPI implements API {
         for (Room room : rooms) {
             if ((room != null) && (price >= 0))
                 if (room.getPrice() >= minPrice && room.getPrice() <= maxPrice)
-                    if ((room.getPersons() == persons) && (room.getCityName() == city) && (room.getHotelName() == hotel)) {
+                    if ((room.getPersons() == persons) && (room.getCityName() == city) && (room.getHotelName() == hotel))
                         index++;
-                    }
+
         }
 
         Room[] rooms2 = new Room[index];
         for (int i = 0; i < rooms.length; i++) {
-            if ((rooms[i] != null) && (price >= 0))
+            if (rooms[i] != null)
                 if (rooms[i].getPrice() >= minPrice && rooms[i].getPrice() <= maxPrice)
-                    if ((rooms[i].getPersons() == persons) && (rooms[i].getCityName() == city) && (rooms[i].getHotelName() == hotel)) {
+                    if ((rooms[i].getPersons() == persons) && (rooms[i].getCityName() == city) && (rooms[i].getHotelName() == hotel))
                         rooms2[i] = rooms[i];
-                    }
         }
         return rooms2;
 

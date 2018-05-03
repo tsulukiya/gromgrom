772c758e5524a6869df9378dc5575fa2adbf0322
macbook <tsulukiya86@gmail.com>
2018-05-03 12:22:47 -0700

    Lesson11(HomeWork)...

--- src/lesson11/homework/BookingComAPI.java
+++ src/lesson11/homework/BookingComAPI.java
@@ -11,26 +11,28 @@ public class BookingComAPI implements API {
     @Override
     public Room[] findRooms(int price, int persons, String city, String hotel) {
         int index = 0;
-        int maxPrice = price + 100;
-        int minPrice = price - 100;
 
         for (Room room : rooms) {
-            if ((room != null) && (price >= 0))
-                if (room.getPrice() >= minPrice && room.getPrice() <= maxPrice)
-                    if ((room.getPersons() == persons) && (room.getCityName() == city) && (room.getHotelName() == hotel))
-                        index++;
-
+            if (room != null && price >= 0 &&
+                    room.getPrice() >= price - 100 &&
+                    room.getPrice() <= price + 100 &&
+                    room.getPersons() == persons &&
+                    room.getCityName() == city &&
+                    room.getHotelName() == hotel)
+                index++;
         }
 
         Room[] rooms2 = new Room[index];
-        for (Room room : rooms) {
-            if (room != null)
-                if (room.getPrice() >= minPrice && room.getPrice() <= maxPrice)
-                    if ((room.getPersons() == persons) && (room.getCityName() == city) && (room.getHotelName() == hotel)) {
-                        rooms2[index - 1] = room;
-                        index--;
-                    }
-        }
+        for (Room room : rooms)
+            if (room != null &&
+                    room.getPrice() >= price - 100 &&
+                    room.getPrice() <= price + 100 &&
+                    room.getPersons() == persons &&
+                    room.getCityName() == city &&
+                    room.getHotelName() == hotel) {
+                rooms2[index - 1] = room;
+                index--;
+            }
         return rooms2;
 
 

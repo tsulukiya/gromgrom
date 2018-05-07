f1c5a50bd281c0c02da257e19a41e9995042e3f2
macbook <tsulukiya86@gmail.com>
2018-05-07 13:22:01 -0700

    Lesson11(HomeWork)...

--- src/lesson11/homework/Controller.java
+++ src/lesson11/homework/Controller.java
@@ -24,11 +24,38 @@ public class Controller {
         }
         return rooms;
     }
+
+
+    public Room[] check(API api1, API api2) {
+        int index = 0;
+        for (Room room : api1.getAll()) {
+            for (Room room1 : api2.getAll()) {
+                if (room.getPrice() == room1.getPrice() &&
+                        room.getHotelName() == room1.getHotelName() &&
+                        room.getCityName() == room1.getCityName() &&
+                        room.getPersons() == room1.getPersons())
+                    index++;
+
+
+            }
+        }
+        Room[] rooms = new Room[index];
+        index = 0;
+        for (Room room : api1.getAll()) {
+            for (Room room1 : api2.getAll()) {
+                if (room.getPrice() == room1.getPrice() &&
+                        room.getHotelName() == room1.getHotelName() &&
+                        room.getCityName() == room1.getCityName() &&
+                        room.getPersons() == room1.getPersons()) {
+                    rooms[index] = room;
+                    index++;
+                }
+
+
+            }
+        }
+        return rooms;
+    }
 }
-
-
-    /*public Room[] check(API api1, API api2) {
-
-    }*/
 
 

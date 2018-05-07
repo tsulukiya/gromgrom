2c7db19e968eca46e249df61d09f878720fc54a1
macbook <tsulukiya86@gmail.com>
2018-05-07 12:36:30 -0700

    Lesson11(HomeWork)...

--- src/lesson11/homework/Controller.java
+++ src/lesson11/homework/Controller.java
@@ -1,19 +1,34 @@
 package lesson11.homework;
 
 public class Controller {
-    /*API[] apis;
+    API[] apis;
 
     public Controller(API[] apis) {
         this.apis = apis;
     }
 
     public Room[] requestRooms(int price, int persons, String city, String hotel) {
+        int index = 0;
 
+        for (API api : apis) {
+            index = index + api.findRooms(price, persons, city, hotel).length;
+        }
+        Room[] rooms = new Room[index];
+        index = 0;
 
-
+        for (API api : apis) {
+            for (Room room : api.findRooms(price, persons, city, hotel)) {
+                rooms[index] = room;
+                index++;
+            }
+        }
+        return rooms;
     }
+}
 
-    public Room[] check(API api1, API api2) {
+
+    /*public Room[] check(API api1, API api2) {
 
     }*/
-}
+
+

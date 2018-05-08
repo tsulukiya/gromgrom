c83cfe0ba12c81e679f8d50ff09871434680c7f7
macbook <tsulukiya86@gmail.com>
2018-05-08 13:01:10 -0700

    Lesson12(HomeWork)...

--- src/lesson12/UkrainianBankSystem.java
+++ src/lesson12/UkrainianBankSystem.java
@@ -12,9 +12,9 @@ public class UkrainianBankSystem implements BankSystem {
 
     @Override
     public void fund(User user, int amount) {
-        if (user.getBank().getLimitOfFunding() >= amount)
-            user.setBalance(user.getBalance() + amount);
-        System.err.println("Can't fund money " + amount + " from user " + user.toString());
+        if (!checkFund(user, amount))
+            return;
+        user.setBalance(user.getBalance() + amount);
     }
 
     @Override
@@ -44,6 +44,10 @@ public class UkrainianBankSystem implements BankSystem {
                 checkWithdrawLimits(user, amount, user.getBalance());
     }
 
+    private boolean checkFund(User user, int amount) {
+        return checkFundLimits(user, amount, user.getBank().getLimitOfFunding());
+    }
+
     private boolean checkWithdrawLimits(User user, int amount, double limit) {
         if (amount + user.getBank().getCommission(amount) > limit) {
             printWithdrawalErrorMsg(amount, user);
@@ -52,8 +56,20 @@ public class UkrainianBankSystem implements BankSystem {
         return true;
     }
 
+    private boolean checkFundLimits(User user, int amount, double limit) {
+        if (amount > limit) {
+            printFundErrorMsg(amount, user);
+            return false;
+        }
+        return true;
+    }
+
     private void printWithdrawalErrorMsg(int amount, User user) {
         System.err.println("Can't withdraw money " + amount + " from user " + user.toString());
+    }
+
+    private void printFundErrorMsg(int amount, User user) {
+        System.err.println("Can't fund money " + amount + " from user " + user.toString());
     }
 
 

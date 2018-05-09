324f4900664dfffc5981680cee12aac686927b27
macbook <tsulukiya86@gmail.com>
2018-05-09 01:57:39 -0700

    Lesson12(HomeWork)...

--- src/lesson12/UkrainianBankSystem.java
+++ src/lesson12/UkrainianBankSystem.java
@@ -20,15 +20,15 @@ public class UkrainianBankSystem implements BankSystem {
     @Override
     public void transferMoney(User fromUser, User toUser, int amount) {
 
-        if (fromUser.getBank() != null && toUser != null)
+        if (fromUser.getBank() != null && toUser.getBank() != null) {
 
             if (!checkTransferMoney(fromUser, toUser, amount))
                 return;
 
-        fromUser.setBalance(fromUser.getBalance() - amount - amount * fromUser.getBank().getCommission(amount));
+            fromUser.setBalance(fromUser.getBalance() - amount - amount * fromUser.getBank().getCommission(amount));
 
-        toUser.setBalance(toUser.getBalance() + amount);
-
+            toUser.setBalance(toUser.getBalance() + amount);
+        }
     }
 
     @Override

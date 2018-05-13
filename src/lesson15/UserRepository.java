bd59af951258d8a5a9889a37dd14b6d5b2e2d53a
macbook <tsulukiya86@gmail.com>
2018-05-13 12:30:26 -0700

    commit firstDebug...

--- src/lesson15/UserRepository.java
+++ src/lesson15/UserRepository.java
@@ -3,8 +3,19 @@ package lesson15;
 import java.util.Arrays;
 
 public class UserRepository {
+    User user1 = new User(10012, "Ann2", "qwe12");
+    User user2 = new User(10013, "Ann3", "qwe13");
+    User user3 = new User(10041, "Ann4", "qwe14");
+    User user4 = new User(10015, "Ann5", "qwe15");
+    User user5 = new User(10061, "Ann6", "qwe16");
+    User user6 = new User(10017, "Ann7", "qwe17");
+    User user7 = new User(10018, "Ann8", "qwe18");
+    User user8 = new User(10019, "Ann9", "qwe19");
+    User user9 = new User(10001, "Ann10", "qwe20");
+    User user10 = null;
 
-    private User[] users = new User[10];
+    private User[] users = new User[]{user1, user2, user3,user4,user5,user6,user7,user8,user9,user10};
+
 
 
     public User[] getUsers() {

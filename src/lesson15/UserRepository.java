ce5b50fe233304044db6ff9728719d6d405d0052
macbook <tsulukiya86@gmail.com>
2018-05-14 12:17:53 -0700

    commit firstDebug...

--- src/lesson15/UserRepository.java
+++ src/lesson15/UserRepository.java
@@ -32,17 +32,17 @@ public class UserRepository {
     }
 
     public User update(User user) {
-        User userSearch = null;
+        //User userSearch = null;
         for (int i = 0; i < users.length; i++) {
             if ((users[i] != null) && (user != null))
                 if (users[i].equals(user)) {
                     users[i] = user;
-                    userSearch = user;
+                    //userSearch = user;
                     break;
                 }
 
         }
-        return userSearch;
+        return user;
 
     }
 

b669c61483b17ea55e43411da61ab29f4573e95a
macbook <tsulukiya86@gmail.com>
2018-05-15 12:50:58 -0700

    commit firstDebug...

--- src/lesson15/UserRepository.java
+++ src/lesson15/UserRepository.java
@@ -43,13 +43,12 @@ public class UserRepository {
         User searchUser = findById(user.getId());
         if (searchUser != null)
             for (int i = 0; i < users.length; i++) {
-                if (searchUser.getId() == users[i].getId()) {
+                if (searchUser.getId() == users[i].getId())
                     if (!users[i].equals(searchUser)) {
-                        users[i] = user;
-                        searchUser = user;
+                        users[i] = searchUser;
                         break;
                     }
-                } else searchUser = null;
+
 
             }
 

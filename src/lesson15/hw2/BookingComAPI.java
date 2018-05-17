090d5635d9290ee61a52caf2da2ee126e406a57b
macbook <tsulukiya86@gmail.com>
2018-05-17 22:08:18 +0300

    commit firstDebug...

---
+++ src/lesson15/hw2/BookingComAPI.java
@@ -0,0 +1,47 @@
+package lesson15.hw2;
+
+public class BookingComAPI implements API {
+
+    private Room[] rooms;
+
+    public BookingComAPI(Room[] rooms) {
+        this.rooms = rooms;
+    }
+
+    @Override
+    public Room[] findRooms(int price, int persons, String city, String hotel) {
+        int index = 0;
+
+        for (Room room : rooms) {
+            if (room != null && price >= 0 &&
+                    room.getPrice() >= price - 100 &&
+                    room.getPrice() <= price + 100 &&
+                    room.getPersons() == persons &&
+                    room.getCityName() == city &&
+                    room.getHotelName() == hotel)
+                index++;
+        }
+
+        Room[] rooms2 = new Room[index];
+        index = 0;
+        for (Room room : rooms)
+            if (room != null &&
+                    room.getPrice() >= price - 100 &&
+                    room.getPrice() <= price + 100 &&
+                    room.getPersons() == persons &&
+                    room.getCityName() == city &&
+                    room.getHotelName() == hotel) {
+                rooms2[index] = room;
+                index++;
+            }
+        return rooms2;
+
+
+    }
+
+    @Override
+    public Room[] getAll() {
+        return rooms;
+    }
+
+}
---
+++ src/lesson15/hw2/Controller.java
@@ -0,0 +1,55 @@
+package lesson15.hw2;
+
+public class Controller {
+    API[] apis;
+
+    public Controller(API[] apis) {
+        this.apis = apis;
+    }
+
+    public Room[] requestRooms(int price, int persons, String city, String hotel) {
+        int index = 0;
+
+        for (API api : apis) {
+            index = index + api.findRooms(price, persons, city, hotel).length;
+        }
+        Room[] rooms = new Room[index];
+        index = 0;
+
+        for (API api : apis) {
+            for (Room room : api.findRooms(price, persons, city, hotel)) {
+                rooms[index] = room;
+                index++;
+            }
+        }
+        return rooms;
+    }
+
+
+    public Room[] check(API api1, API api2) {
+        int index = 0;
+        for (Room room : api1.getAll()) {
+            for (Room room1 : api2.getAll()) {
+                if (room.equals(room1))
+                    index++;
+
+
+            }
+        }
+        Room[] rooms = new Room[index];
+        index = 0;
+        for (Room room : api1.getAll()) {
+            for (Room room1 : api2.getAll()) {
+                if (room.equals(room1)) {
+                    rooms[index] = room;
+                    index++;
+                }
+
+
+            }
+        }
+        return rooms;
+    }
+}
+
+

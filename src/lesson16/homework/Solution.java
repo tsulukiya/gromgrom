d00d167d8002bd8d37c68b3f324539e49e109228
macbook <tsulukiya86@gmail.com>
2018-05-20 23:13:09 +0300

    commit firstDebug...

--- src/lesson16/homework/Solution.java
+++ src/lesson16/homework/Solution.java
@@ -1,8 +1,33 @@
 package lesson16.homework;
 
 public class Solution {
+    public static void main(String[] args) {
 
-    int countWords(String input) {
-        return input.split(" ").length;
+        String s1 = " Hello World";
+        System.out.println(countWords(s1));
+        System.out.println(s1.length());
+
     }
+
+//    public static int countWords(String input) {
+//        return input.split(" ").length;
+//    }
+
+    public static int countWords(String input) {
+
+        int count = 0;
+
+        char [] chars = input.toCharArray();
+
+        for (char ch : chars) {
+            if (ch == ' ')
+                count++;
+
+        }
+        if (chars[0] != ' ')
+            return count+1;
+        return count;
+    }
+
+
 }

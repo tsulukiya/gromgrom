8d5c99e0fce6b103081c0aa1ec55014dbc0ff540
macbook <tsulukiya86@gmail.com>
2018-05-29 22:29:18 +0300

    commit firstDebug...

--- src/lesson16/homework/Solution.java
+++ src/lesson16/homework/Solution.java
@@ -8,6 +8,8 @@ public class Solution {
         String test = "Hello World is first program";
 
         System.out.println(countWords(test));
+        System.out.println(maxWord(test));
+        System.out.println(minWord(test));
 
 
     }
@@ -30,5 +32,49 @@ public class Solution {
         }
 
         return count;
+    }
+
+    public static String maxWord(String input) {
+
+        String [] mas = input.split(" ");
+        String maxWord = mas[0];
+        int count = 0;
+
+        for (String ma : mas) {
+            count = 0;
+            for (char c : ma.toCharArray()) {
+                if (Character.isLetter(c))
+                    count++;
+                if (count == ma.toCharArray().length)
+                    if (ma.toCharArray().length > maxWord.length())
+                        maxWord = ma;
+            }
+
+        }
+        return maxWord;
+
+
+    }
+
+    public static String minWord(String input) {
+
+        String [] mas = input.split(" ");
+        String minWord = mas[0];
+        int count = 0;
+
+        for (String ma : mas) {
+            count = 0;
+            for (char c : ma.toCharArray()) {
+                if (Character.isLetter(c))
+                    count++;
+                if (count == ma.toCharArray().length)
+                    if (ma.toCharArray().length < minWord.length())
+                        minWord = ma;
+            }
+
+        }
+        return minWord;
+
+
     }
 }

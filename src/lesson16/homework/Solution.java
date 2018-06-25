c49026b0e96ea48de4023ee3878219bc63da5ee8
macbook <tsulukiya86@gmail.com>
2018-06-25 13:48:05 +0300

    commit firstDebug...

--- src/lesson16/homework/Solution.java
+++ src/lesson16/homework/Solution.java
@@ -1,16 +1,15 @@
 package lesson16.homework;
 
-import java.util.Arrays;
-
 public class Solution {
     public static void main(String[] args) {
 
         //String test = "First program in JAVA";
-        String test = "";
+        String test = "aaa";
 
         System.out.println(countWords(test));
         System.out.println(maxWord(test));
         System.out.println(minWord(test));
+        System.out.println(mostCountedWord(test));
 
 
     }
@@ -72,7 +71,35 @@ public class Solution {
         }
         return minWord;
     }
-        
+
+
+    public static String mostCountedWord(String input) {
+        String[] mas = input.split(" ");
+        String repeatWord = isWord(input);
+        int[] count = new int[mas.length];
+
+        for (int i = 0; i < mas.length; i++) {
+            for (int j = 0; j < mas.length; j++) {
+                if (mas[i].equals(mas[j])) {
+                    count[i]++;
+                }
+            }
+        }
+
+        for (int i = 0; i < count.length; i++) {
+            for (int j = 0; j < count.length; j++) {
+                if (count[i]<count[j]) {
+                    repeatWord = mas[j];
+                }
+            }
+        }
+
+
+
+        return repeatWord;
+
+    }
+
 
     private static String isWord(String input) {
 
@@ -93,4 +120,6 @@ public class Solution {
         }
         return word;
     }
+
+
 }

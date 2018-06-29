da9626b26e7b1b5ee4e85f6d1831d0b783effd1d
macbook <tsulukiya86@gmail.com>
2018-06-29 20:41:12 +0300

    commit firstDebug...

--- src/lesson16/homework/Solution.java
+++ src/lesson16/homework/Solution.java
@@ -9,18 +9,28 @@ public class Solution {
         String test2 = "1";
         String test3 = "aaaa aa1";
         String test4 = "aaaa% @@@@ @@@@";
+        String test5 = "http://www.westside..org";
 
         //System.out.println(countWords(test));
         //System.out.println(maxWord(test));
         //System.out.println(minWord(test));
-        System.out.println(mostCountedWord(test));
-        System.out.println(mostCountedWord(test1));
-        System.out.println(mostCountedWord(test2));
-        System.out.println(mostCountedWord(test3));
-        System.out.println(mostCountedWord(test4));
+//        System.out.println(mostCountedWord(test));
+//        System.out.println(mostCountedWord(test1));
+//        System.out.println(mostCountedWord(test2));
+//        System.out.println(mostCountedWord(test3));
+//        System.out.println(mostCountedWord(test4));
+        System.out.println(validate(test5));
+        System.out.println(checkDomainName(test5));
+        System.out.println(checkProtocol(test5));
+        System.out.println(checkSiteName(test5));
 
 
     }
+
+    public static boolean validate(String address) {
+        return checkSiteName(address);
+    }
+
 
     public static int countWords(String input) {
         int count = 0;
@@ -124,6 +134,86 @@ public class Solution {
 
         return repeatWord;
 
+    }
+
+    private static String checkProtocol(String address) {
+        String p = "http://";
+        String p1 = "https://";
+        char[] chars = address.toCharArray();
+        String check7 = "";
+        String check8 = "";
+
+        for (int i = 0; i < 7; i++) {
+            check7 += chars[i];
+        }
+
+        for (int i = 0; i < 8; i++) {
+            check8 += chars[i];
+        }
+
+        if (check7.equals(p))
+            return check7;
+        if (check8.equals(p1))
+            return check8;
+        return null;
+
+    }
+
+    private static boolean checkSiteName(String address) {
+
+        char[] chars = address.toCharArray();
+        String check7 = "";
+        String protocol = checkProtocol(address);
+        String domain = checkDomainName(address);
+
+        if ((chars[protocol.length()] == 'w' &&
+                chars[protocol.length() + 1] == 'w' &&
+                chars[protocol.length() + 2] == 'w') &&
+                chars[protocol.length() + 3] == '.') {
+            for (int i = protocol.length() + 4; i < chars.length - domain.length(); i++) {
+                check7 += chars[i];
+            }
+
+        } else {
+            for (int i = protocol.length(); i < chars.length - domain.length(); i++) {
+                if (Character.isLetter(chars[i]))
+                    check7 += chars[i];
+
+            }
+        }
+
+        char [] chars1 = check7.toCharArray();
+        int m = 0;
+
+        for (char c : chars1) {
+            if (Character.isLetter(c))
+                m++;
+        }
+
+        return (chars1.length == m);
+
+
+
+    }
+
+    private static String checkDomainName(String address) {
+        String domain1 = ".com";
+        String domain2 = ".org";
+        String domain3 = ".net";
+        char[] chars = address.toCharArray();
+        String check7 = "";
+
+        for (int i = chars.length - 4; i < chars.length; i++) {
+            check7 += chars[i];
+        }
+
+        if (check7.equals(domain1))
+            return domain1;
+        if (check7.equals(domain2))
+            return domain2;
+        if (check7.equals(domain3))
+            return domain3;
+        return null;
     }
 
 

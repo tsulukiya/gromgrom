56a0cbee99c988f62c6b3475093347c31b821d46
macbook <tsulukiya86@gmail.com>
2018-10-28 21:50:34 +0200

    lesson 29 (HomeWork exc.)

---
+++ src/lesson29/Order.java
@@ -0,0 +1,56 @@
+package lesson29;
+
+public class Order {
+    private long id;
+    private int price;
+    private String currency;
+    private String itemName;
+    private String shopIdentificator;
+
+    public Order(long id, int price, String currency, String itemName, String shopIdentificator) {
+        this.id = id;
+        this.price = price;
+        this.currency = currency;
+        this.itemName = itemName;
+        this.shopIdentificator = shopIdentificator;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", price=" + price +
+                ", currency='" + currency + '\'' +
+                ", itemName='" + itemName + '\'' +
+                ", shopIdentificator='" + shopIdentificator + '\'' +
+                '}' + '\n';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Order order = (Order) o;
+
+        if (id != order.id) return false;
+        if (price != order.price) return false;
+        if (!currency.equals(order.currency)) return false;
+        if (!itemName.equals(order.itemName)) return false;
+        return shopIdentificator.equals(order.shopIdentificator);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + price;
+        result = 31 * result + currency.hashCode();
+        result = 31 * result + itemName.hashCode();
+        result = 31 * result + shopIdentificator.hashCode();
+        return result;
+    }
+
+    public long getId() {
+        return id;
+    }
+}

5cebdb1f57757e28e832d5b0e68a3f21fb8edb73
macbook <tsulukiya86@gmail.com>
2018-11-26 21:17:57 +0200

    lesson 28 (HomeWork exc.) - check nullPointer

---
+++ src/lesson30/Currency.java
@@ -0,0 +1,6 @@
+package lesson30;
+
+public enum Currency {
+    USD,
+    EUR
+}
---
+++ src/lesson30/UkrainianBankSystem.java
@@ -0,0 +1,82 @@
+package lesson30;
+
+public class UkrainianBankSystem implements BankSystem {
+
+    @Override
+    public void withdraw(User user, int amount) {
+        if (!checkWithdraw(user, amount))
+            return;
+        user.setBalance(user.getBalance() - amount - amount * user.getBank().getCommission(amount));
+    }
+
+    @Override
+    public void fund(User user, int amount) {
+        if (!checkFund(user, amount))
+            return;
+        user.setBalance(user.getBalance() + amount);
+    }
+
+    @Override
+    public void transferMoney(User fromUser, User toUser, int amount) {
+        if (!checkTransferMoney(fromUser, toUser, amount))
+            return;
+        fromUser.setBalance(fromUser.getBalance() - amount - amount * fromUser.getBank().getCommission(amount));
+        toUser.setBalance(toUser.getBalance() + amount);
+    }
+
+    @Override
+    public void paySalary(User user) {
+        if (!checkFund(user, user.getSalary()))
+            return;
+        user.setBalance(user.getBalance() + user.getSalary());
+
+    }
+
+    private boolean checkWithdraw(User user, int amount) {
+        return checkWithdrawLimits(user, amount, user.getBank().getLimitOfWithdrawal()) &&
+                checkWithdrawLimits(user, amount, user.getBalance());
+    }
+
+    private boolean checkFund(User user, int amount) {
+        return checkFundLimits(user, amount, user.getBank().getLimitOfFunding());
+    }
+
+    private boolean checkWithdrawLimits(User user, int amount, double limit) {
+        if (amount + user.getBank().getCommission(amount) > limit) {
+            printWithdrawalErrorMsg(amount, user);
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkFundLimits(User user, int amount, double limit) {
+        if (amount > limit) {
+            printFundErrorMsg(amount, user);
+            return false;
+        }
+        return true;
+    }
+
+    private void printWithdrawalErrorMsg(int amount, User user) {
+        System.err.println("Can't withdraw money " + amount + " from user " + user.toString());
+    }
+
+    private void printFundErrorMsg(int amount, User user) {
+        System.err.println("Can't fund money " + amount + " from user " + user.toString());
+    }
+
+    private boolean checkAmountFund(int amount) {
+        if (amount <= 0) {
+            System.err.println(" ERROR: Value 'amount' isn't correct...");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkTransferMoney(User fromUser, User toUser, int amount) {
+        return checkAmountFund(amount) && checkWithdraw(fromUser, amount) && checkFund(toUser, amount) &&
+                (fromUser.getBank().getCurrency() == toUser.getBank().getCurrency());
+    }
+
+
+}

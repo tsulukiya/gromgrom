fa4a9a580fe49d3ee123f6a10332d5917ad4e58c
macbook <tsulukiya86@gmail.com>
2018-12-09 13:49:18 +0200

    lesson 30 ( task2) commit...

--- src/lesson30/task2/ProjectDAO.java
+++ src/lesson30/task2/ProjectDAO.java
@@ -1,14 +1,10 @@
 package lesson30.task2;
 
-import java.util.HashSet;
 import java.util.Set;
 
 public class ProjectDAO {
 
     public static Set<Project> projectByEmployee(Employee employee) {
-        Set<Project> projects1 = new HashSet<>();
-        if (employee!=null)
-        projects1.addAll(employee.getProjects());
-        return projects1;
+        return employee.getProjects();
     }
 }

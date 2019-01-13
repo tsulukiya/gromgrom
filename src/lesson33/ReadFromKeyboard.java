50d250143b218a2de7567393dc872cfd87027489
macbook <tsulukiya86@gmail.com>
2019-01-13 22:33:50 +0200

    lesson 32 ( task2) with validate commit...

--- src/lesson33/ReadFromKeyboard.java
+++ src/lesson33/ReadFromKeyboard.java
@@ -1,5 +1,7 @@
 package lesson33;
 
+import org.apache.commons.io.IOUtils;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,15 +10,46 @@ public class ReadFromKeyboard {
 
     public static void main(String[] args) throws IOException {
 
+        readKeyboardWithIOStream1();
+        readKeyboardWithIOStream2();
+
     }
 
-    private static void readKeyboardWithIOStream() throws IOException {
+    private static void readKeyboardWithIOStream1() throws IOException {
         InputStreamReader reader = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(reader);
 
         System.out.println("Please enter your name");
 
-        System.out.println("Hello, " + br.readLine() + "!");
+        try {
+            System.out.println("Hello, " + br.readLine() + "!");
+        } catch (IOException e) {
+            System.out.println("Reading from keyboard failed");
+        } finally {
+            try {
+                reader.close();
+                br.close();
+            } catch (IOException e) {
+                System.err.println("Closing streams failed");
+            }
+        }
+    }
+
+
+    private static void readKeyboardWithIOStream2() throws IOException {
+        InputStreamReader reader = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(reader);
+
+        System.out.println("Please enter your name");
+
+        try {
+            System.out.println("Hello, " + br.readLine() + "!");
+        } catch (IOException e) {
+            System.out.println("Reading from keyboard failed");
+        } finally {
+            IOUtils.closeQuietly(reader);
+            IOUtils.closeQuietly(br);
+        }
     }
 
 }

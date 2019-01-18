908a7818d5e4fa6b1fdd19121f61ff6f92241109
macbook <tsulukiya86@gmail.com>
2019-01-18 19:09:41 +0200

    lesson34(task1...)

--- src/lesson34/task1/Solution.java
+++ src/lesson34/task1/Solution.java
@@ -4,13 +4,11 @@ import java.io.*;
 
 public class Solution {
 
-    public static void copyFileContent(String fileFromPath, String fileToPath) throws FileNotFoundException {
+    public static void copyFileContent(String fileFromPath, String fileToPath) throws Exception {
         File fileFrom = new File(fileFromPath);
         File fileTo = new File(fileToPath);
 
-        if (!fileFrom.exists() || !fileTo.exists()) {
-            throw new FileNotFoundException("File does not exist");
-        }
+        validate(fileFrom, fileTo);
 
         try (FileReader fileReader = new FileReader(fileFrom);
              BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -19,18 +17,38 @@ public class Solution {
 
             String text;
 
-            while ((text = bufferedReader.readLine())!=null) {
+            while ((text = bufferedReader.readLine()) != null) {
 
                 bufferedWriter.write(text);
                 bufferedWriter.append("\n");
 
             }
 
-        }catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             System.out.println(e.getMessage() + fileFrom.getName());
-        }catch (IOException e) {
+        } catch (IOException e) {
             System.out.println(e.getMessage() + fileFrom.getName());
         }
 
+    }
+
+
+    private static void validate(File fileFrom, File fileTo) throws Exception {
+
+        if (!fileFrom.exists()) {
+            throw new FileNotFoundException("File " + fileFrom + "does not exist");
+        }
+
+        if (!fileTo.exists()) {
+            throw new FileNotFoundException("File " + fileTo + "does not exist");
+        }
+
+        if (!fileFrom.canRead()) {
+            throw new Exception("File " + fileFrom + " does not have permission to be read");
+        }
+
+        if (!fileTo.canWrite()) {
+            throw new Exception("File " + fileTo + " does not have permission to be written");
+        }
     }
 }

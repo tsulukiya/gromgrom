65245e6e6202aed617bbdd857acef822a99682a2
macbook <tsulukiya86@gmail.com>
2019-01-21 00:14:27 +0200

    lesson34(task2...)

---
+++ src/lesson34/task3/Demo.java
@@ -0,0 +1,10 @@
+package lesson34.task3;
+
+public class Demo {
+
+    public static void main(String[] args) throws Exception {
+
+        Solution.transferSentences("/Users/macbook/Documents/testFrom.txt",
+                "/Users/macbook/Documents/testTo.txt", "Year");
+    }
+}
